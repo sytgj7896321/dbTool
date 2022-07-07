0a3535e20bfef5a183c48e15db3fe8a6dcfbd5f2
Yi-tao Shi <yi-tao.shi@hp.com>
2022-07-07 09:06:26 +0800

    Done

--- relation/connect.go
+++ relation/connect.go
@@ -13,7 +13,8 @@ type MyDB struct {
 func (m *MyDB) OpenDB() error {
 	var err error
 	m.DB, err = sql.Open(cmd.RDBType, cmd.RDBConnection)
-	m.DB.SetConnMaxLifetime(3 * time.Minute)
+	m.DB.SetConnMaxLifetime(-1 * time.Minute)
+	m.DB.SetConnMaxIdleTime(time.Minute)
 	m.DB.SetMaxOpenConns(100)
 	m.DB.SetMaxIdleConns(100)
 	return err
